8c271d44f774b5e6ffacff906722bddf8416f18e
Guifendjy <guifendjy123@gmail.com>
2025-01-30 04:25:21 -0500

    initial commit.

---
+++ src/App.js
@@ -0,0 +1,267 @@
+import dommaster from "/dom.svg";
+import vite from "/vite.svg";
+
+import { setupRecorder, formatTime, findMatchingBlob } from "./utils";
+
+const { ReactiveState, createElement } = master; // from CDN link
+
+let recorder = setupRecorder(); // returns a promise
+
+const state = new ReactiveState({
+  // logo used in template
+  dommaster,
+  vite,
+  // template logic
+  recordings: [],
+  error: null,
+  isRecording: false,
+  isPlaying: false,
+  dark: true,
+  audio: null,
+  canvasContainer: null,
+  currentTime: "0:00",
+  record() {
+    if (recorder.error) {
+      this.error = recorder.error;
+      return;
+    } // to not work when if media devices or not accessible.
+    this.isRecording = !this.isRecording;
+    this.audioURL = null; // reset every time
+    let timer = 0;
+    let interval;
+    this.currentTime = "0:00";
+
+    if (this.isRecording) {
+      interval = setInterval(() => {
+        if (!this.isRecording) {
+          clearInterval(interval);
+          timer = 0;
+          return;
+        }
+        this.currentTime = `${formatTime(timer)}`;
+        timer += 1;
+      }, 1000);
+    }
+
+    recorder.then(({ stopRecording, startRecording }) => {
+      if (this.isRecording) {
+        startRecording();
+      } else {
+        stopRecording((audio, blob) => {
+          this.audio = audio;
+
+          this.audio.ontimeupdate = () => {
+            if (audio.duration && audio.duration == audio.currentTime) {
+              this.isPlaying = false;
+            }
+            this.currentTime = `${formatTime(audio.currentTime)}`;
+          };
+
+          this.audio.load();
+
+          if (this.recordings.length) {
+            this.recordings = this.recordings = this.recordings.map(
+              (rec, i) => {
+                rec.selected = false;
+                return rec;
+              }
+            );
+          }
+          let data = {
+            selected: true,
+            title: `untitled_${this.recordings.length + 1}.ogg`,
+            blob,
+            src: URL.createObjectURL(blob),
+            downloaded: false,
+          };
+
+          this.recordings = [...this.recordings, data];
+        });
+      }
+    });
+  },
+  play() {
+    if (!this.audio) return; // can't be played if there are no url
+    this.isPlaying = !this.isPlaying;
+
+    if (this.audio && this.isPlaying) {
+      this.audio.play();
+    } else {
+      this.audio.pause();
+    }
+  },
+  toggleTheme() {
+    this.dark = !this.dark;
+  },
+  closeError() {
+    this.error = null;
+  },
+  setAudioSrc(index) {
+    if (this.isPlaying) {
+      this.error =
+        "can't switch recording while one is playing. pause it first.";
+      return;
+    }
+    if (this.audio) {
+      this.recordings = this.recordings.map((rec, i) => {
+        rec.selected = false;
+        if (i == index) {
+          rec.selected = true;
+        }
+        return rec;
+      });
+
+      let src = this.recordings[index].src;
+      this.audio.src = URL.createObjectURL(src);
+    }
+  },
+  select(e) {
+    const target = e.target;
+    const valueCount = target.value.split(".")[0].length;
+    target.setSelectionRange(0, valueCount);
+    target.focus();
+  },
+  changeTitle(index, e) {
+    const value = e.target.value;
+    this.recordings = this.recordings.map((rec, i) => {
+      if (i == index) rec.title = value;
+      return rec;
+    });
+  },
+  download(index) {
+    let { src, title, downloaded } = this.recordings.find((_, i) => i == index);
+    this.recordings = this.recordings.map((rec, i) => {
+      if (i == index) {
+        rec.downloaded = true;
+      }
+      return rec;
+    });
+
+    if (downloaded) return (this.error = "recording recently downloaded");
+
+    const link = document.createElement("a");
+    link.href = src;
+    link.download = title;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+    // allow dowload after a few seconds
+    setTimeout(() => {
+      this.recordings = this.recordings.map((rec, i) => {
+        if (i == index) {
+          rec.downloaded = false;
+        }
+        return rec;
+      });
+    }, 3000);
+  },
+  async remove(index) {
+    let { blob, src } = this.recordings.find((_, i) => i === index);
+    let isCurrentAudio = await findMatchingBlob(this.audio, blob);
+    if (this.isPlaying && isCurrentAudio) {
+      this.error =
+        "Can't remove recording while it's playing. pause or stop first.";
+      return;
+    }
+
+    URL.revokeObjectURL(src);
+    this.recordings = this.recordings.filter((_, i) => i != index);
+  },
+});
+
+// error if not running on localhost or is secured
+const setTheme = () => {
+  document.body.setAttribute("data-theme", `${state.dark ? "dark" : "light"}`);
+};
+
+state.subscribe("dark", setTheme);
+setTheme(); // initial call
+
+const template = /*html*/ `
+<div class="main">
+  <div style="display:{error ? 'flex': 'none'};" class="error-dialog">
+     <div class="error-icon">
+       <span>!</span>
+     </div>
+     <div class="error-content">
+       <h3>Error</h3>
+       <p>{error}</p>
+     </div>
+     <button onclick="closeError" class="error-close">✕</button>
+   </div>
+
+  <!-- From Uiverse.io by Praashoo7 -->
+  <label class="switch">
+    <input onchange="toggleTheme" type="checkbox">
+    <span class="slider"></span>
+  </label>
+<span style="font-family:Montserrat; font-size:0.8em; margin-top:-15px">{dark ? 'dark': 'light'} mode</span>
+  <h1>Voice Recorder</h1>
+  {start:if recordings.length}
+    <div class="list">
+    {for:each recording, index of recordings}
+      <div class="recording">
+        <span onclick="setAudioSrc(index)" class="recording-logo {recording.selected ? 'selected' : ''}">🔴</span>
+        <input onmouseenter="select" onchange="changeTitle(index)" class="recording-title" type="text" value="{recording.title}"/>
+        <button onclick="download(index)" class="download-btn">
+         {!recording.downloaded ? '&#x2B07;' : '&#10003;'}
+        </button>
+        <button onclick="remove(index)" class="download-btn">
+          &#10005;
+        </button>
+      </div>
+    {end:each}
+  </div>
+  {end:if}
+  <div class="visualizer">
+  <div class="top">
+      {start:if isPlaying || isRecording}
+          <div class="top-content">
+            <span class="fade-in-out">{isRecording ? "REC" : isPlaying ? "PLAY": ""}</span>
+            <span>{currentTime}</span>
+          </div>
+      {end:if}
+    </div>
+    <div class="middle"></div>
+  </div>
+  <div class="buttons">
+    <div class="button_pair">
+      <div class="btn">
+        <button disabled="{isPlaying}"  onclick="record" class="button3">
+          <span class="button_text">{!isRecording ? 'RECORD' : 'STOP'}</span>
+        </button>
+      </div>
+      <div class="btn">
+        <button disabled="{isRecording  || !audio || recordings.length < 1}" onclick="play" class="button4">
+          <span class="button_text">{!isPlaying ? 'PLAY' : 'PAUSE'}</span>
+        </button>
+      </div>
+    </div>
+  </div>
+
+  <div class="footer">
+      <h2>Vite + Dom Master</h2>
+      <a href="https://vite.dev/" target="_blank">
+        <img src="{vite}" alt="vite logo" />
+      </a>
+      <a href="https://github.com/guifendjy/dom_master" target="_blank">
+        <img width="50" height="50" src="{dommaster}" alt="dommaster logo" />
+      </a>
+  </div>
+  <span style="margin-top:-30px; font-size:0.80rem; color:gray;">click any of the logos to learn more.</span>
+</div>
+`;
+
+let App = createElement(template, state);
+let visualizerContainer = App.querySelector(".middle");
+
+if (!recorder.error) {
+  recorder.then(({ canvas }) => {
+    canvas.style.borderBottom = "1px solid #d42a02";
+    visualizerContainer.appendChild(canvas);
+  });
+} else {
+  state.error = recorder.error;
+}
+export default App;
---
+++ src/utils.js
@@ -0,0 +1,193 @@
+let recorder;
+let dataChunks = [];
+let recorderCallbacks = { src: null };
+
+let recordingAudioContext;
+let playbackAudioContext;
+
+let analyzer;
+let bufferLength;
+let dataArray;
+
+let clearAnimation;
+
+export function setupRecorder() {
+  let response;
+  try {
+    response = navigator.mediaDevices
+      .getUserMedia({
+        audio: true,
+      })
+      .then((stream) => {
+        return setupStream(stream);
+      })
+      .catch((err) => {
+        console.error("Error setting up recorder", err);
+        return { error: "Error setting up recorder" };
+      });
+  } catch (err) {
+    response = {
+      error: "failed to setup recorder. failed to access media devices.",
+    };
+  }
+  return response;
+}
+
+function setupStream(stream) {
+  recorder = new MediaRecorder(stream);
+  const { canvas, draw } = setUpVisualizer(); // gets a stream an returns cavans
+  const audioID = uniid();
+
+  if (recorder) {
+    recorder.ondataavailable = (e) => {
+      dataChunks.push(e.data); // needs somewhere to save the recorded chunks
+    };
+
+    recorder.onstart = (e) => {
+      if (recorderCallbacks.onstartCallback) {
+        recorderCallbacks.onstartCallback(e);
+      }
+    };
+
+    recorder.onstop = (e) => {
+      const blob = new Blob(dataChunks, { type: "audio/ogg; codecs=opus" });
+      dataChunks = [];
+      let blobURL = window.URL.createObjectURL(blob);
+      const audio = new Audio(blobURL);
+
+      audio.controls = true;
+      audio.hidden = true;
+      audio.preload = "metadata";
+      audio.id = audioID;
+
+      let oldAudio = document.getElementById(audioID);
+      if (!oldAudio) document.body.appendChild(audio);
+      else oldAudio.replaceWith(audio);
+
+      playbackAudioContext = new AudioContext();
+      const source = playbackAudioContext.createMediaElementSource(audio);
+      analyzer = playbackAudioContext.createAnalyser();
+      source.connect(analyzer);
+      analyzer.connect(playbackAudioContext.destination);
+
+      setupAnalyzer();
+
+      audio.onplay = () => {
+        draw();
+      };
+      audio.onended = () => {
+        clearAnimation();
+      };
+      audio.onpause = () => {
+        clearAnimation();
+      };
+
+      if (recorderCallbacks.onstop) {
+        recorderCallbacks.onstop(audio, blob);
+      }
+    };
+  }
+
+  let startRecording = (onstartCallback) => {
+    recordingAudioContext = new AudioContext();
+    const source = recordingAudioContext.createMediaStreamSource(stream);
+    analyzer = recordingAudioContext.createAnalyser();
+    source.connect(analyzer);
+
+    setupAnalyzer();
+
+    recorder.start();
+    if (onstartCallback) {
+      recorderCallbacks.onstart = onstartCallback;
+    }
+    draw();
+  };
+
+  let stopRecording = (onstopCallback) => {
+    recorder.stop();
+    recordingAudioContext.close();
+    recordingAudioContext = null;
+
+    if (clearAnimation) {
+      clearAnimation();
+    }
+    if (onstopCallback) {
+      recorderCallbacks.onstop = onstopCallback;
+    }
+  };
+
+  if (!recorder) return null;
+  return { startRecording, stopRecording, canvas };
+}
+
+function setupAnalyzer() {
+  analyzer.fftSize = 256;
+  bufferLength = analyzer.frequencyBinCount;
+  dataArray = new Uint8Array(bufferLength);
+}
+
+function setUpVisualizer() {
+  const canvas = document.createElement("canvas");
+  const canvasCtx = canvas.getContext("2d");
+
+  // default sizing
+  canvas.width = 350;
+  canvas.height = 150;
+
+  function draw() {
+    analyzer.getByteFrequencyData(dataArray);
+
+    const barWidth = (canvas.width / bufferLength) * 2.5;
+    let barheight;
+    let x = 0;
+    canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < bufferLength; i++) {
+      barheight = dataArray[i];
+
+      canvasCtx.fillStyle = "#d42a02";
+      canvasCtx.fillRect(x, canvas.height - barheight / 2, barWidth, barheight);
+      x += barWidth + 1;
+    }
+    let animationId = requestAnimationFrame(draw);
+    clearAnimation = () => {
+      cancelAnimationFrame(animationId);
+      canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+    };
+  }
+  return { canvas, draw };
+}
+
+export function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = Math.floor(seconds) % 60;
+  return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+}
+
+async function getBlobFromAudioSrc(audio) {
+  const response = await fetch(audio.src);
+  return response.blob();
+}
+
+async function areBlobsSame(blob1, blob2) {
+  const buffer1 = await blob1.arrayBuffer();
+  const buffer2 = await blob2.arrayBuffer();
+
+  return (
+    buffer1.byteLength === buffer2.byteLength &&
+    new Uint8Array(buffer1).every(
+      (byte, i) => byte === new Uint8Array(buffer2)[i]
+    )
+  );
+}
+
+export async function findMatchingBlob(audio, savedBlob) {
+  const audioBlob = await getBlobFromAudioSrc(audio);
+  const areSame = await areBlobsSame(audioBlob, savedBlob);
+  return areSame;
+}
+
+function uniid() {
+  let random = parseFloat(Math.random() * 0.999999);
+  return random.toString(36).substring(2, 12);
+}
