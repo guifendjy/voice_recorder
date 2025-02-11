1f76a246ebe36e1eaccf68fc227b55f8b8d965b5
Guifendjy <guifendjy123@gmail.com>
2025-02-11 12:34:49 -0500

    added top level await support.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/voice_recorder/",
+  build: {
+    target: "esnext", // ensure support for top-level await
+  },
   esbuild: {
     target: "esnext", // ensure support for top-level await
   },
