630d220bed94ecb62cc6321186e23bd7d666097c
Guifendjy <guifendjy123@gmail.com>
2025-02-12 08:20:55 -0500

    added recorder favicon

--- src/utils.js
+++ src/utils.js
@@ -13,7 +13,7 @@ let clearAnimation;
 
 export function setupRecorder({ audioType = "audio/webm" } = {}) {
   let response;
-  try {
+  try {npm
     response = navigator.mediaDevices
       .getUserMedia({
         audio: true,
